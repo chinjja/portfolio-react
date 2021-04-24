7d39fc89886d25377b62f0287708d3a93d492879
chinjja <chinjja@gmail.com>
2021-04-24 12:02:18 +0900

    update

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { AppBar, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, IconButton, Link, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Collapse, Container, Grid, IconButton, Link, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@material-ui/core';
 import banner from '../images/banner.jpg'
 import me from '../images/me.jpg'
 import { Variant } from '@material-ui/core/styles/createTypography';
+import { ExpandMore } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -33,6 +34,20 @@ const useStyles = makeStyles((theme) => ({
         marginTop: "16px",
         marginBottom: "16px",
     },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    }
   }));
 
 export function View() {
@@ -43,7 +58,7 @@ export function View() {
             <header>
                 <AppBar position="fixed">
                     <Toolbar>
-                        <Typography variant="body1">😀안녕하세요, 박정현입니다!👋</Typography>
+                        <Typography variant="h6">😀안녕하세요, 박정현입니다!👋</Typography>
                     </Toolbar>
                 </AppBar>
             </header>
@@ -85,33 +100,44 @@ export function View() {
                             {TypoLink("📧 Email", "chinjja@gmail.com", true)}
                             {TypoLink("🐱 GitHub", "https://github.com/chinjja")}
                             {TypoLink("📚 BOJ", "https://www.acmicpc.net/user/chinjja")}
-                            {TypoLink("🍀 Naver Blog", "http://www.naver.com")}
-                            {TypoLink("📷 Instagram", "http://www.naver.com")}
+                            {/* {TypoLink("🍀 Naver Blog", "https://www.naver.com")} */}
+                            {/* {TypoLink("📷 Instagram", "https://www.instagram.com")} */}
                             <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fchinjja%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
                         </Grid>
 
                         <Grid item xs={12}>
-                            {Description("Mobile App Developer", [
+                            {Description("App Developer", [
                                 "꼼꼼한 개발자",
                                 "빈 틈을 채워나가는 개발자",
                                 "사람들이 필요로 하는 것이 무엇인지 고민하는 개발자",
                             ])}
                         </Grid>
 
-                        <Grid item xs={12}>
+                        {/* <Grid item xs={12}>
                             {Subtitle('🏆 Awards')}
                             {Awards()}
-                        </Grid>
+                        </Grid> */}
                         
                         <Grid item xs={12}>
                             {Subtitle('🛠️ Stacks')}
                             {Stacks()}
                         </Grid>
                         
-                        <Grid item xs={12}>
+                        {/* <Grid item xs={12}>
                             {Subtitle('🎒 Activities')}
                             {Activities()}
+                        </Grid> */}
+                        
+                        <Grid item xs={12}>
+                            {Subtitle('👩‍💻 Projects')}
+                            {Projects()}
                         </Grid>
+
+                        {/* <Grid item xs={12}>
+                            {Subtitle('👩‍🏫 Education')}
+                            {Education()}
+                        </Grid> */}
+
                     </Grid>
                 </Container>
 
@@ -177,12 +203,11 @@ function Stacks() {
         <>
         {Description("Java", [
             "Java를 사용해여 Android 앱을 개발해 본 경험이 있습니다.",
-            "객체지향 프로그래밍에 대해 이해하고 있습니다.",
-            "Java에서 제공하는 클래스와 메소드를 적절하게 사용할 수 있습니다.",
+            "Swing을 사용하여 프로젝트를 개발했습니다.",
+            "Java를 사용해서 생각을 구현으로 이끌어 낼 수 있습니다.",
         ])}
         {Description("Git", [
             "Git을 활용한 소스코드 버전 관리 및 협업을 할 수 있습니다.",
-            "GitHub, Gitlab 등을 사용하여 프로젝트 관리 경험이 있습니다.",
         ])}
         </>
     )
@@ -224,6 +249,101 @@ function Activities() {
             </Grid>
         </Grid>
         </>
+    )
+}
+
+function Projects() {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false)
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+    return (
+        <>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+                <CollapseCard title='Shrimp and Chorizo Raella' subheader='September 14, 2016' content={
+                    <>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 100
+                        minutes.
+                    </Typography>
+                    </>
+                }/>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <CollapseCard title='hello' subheader='world' content={
+                    <>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 100
+                        minutes.
+                    </Typography>
+                    </>
+                }/>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <CollapseCard title='hello' subheader='world' content={<Typography>HI</Typography>}/>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
+
+interface CollapseCardProps {
+    title: string,
+    subheader: string,
+    content: JSX.Element,
+}
+
+function CollapseCard(props: CollapseCardProps) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false)
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+    return (
+        <>
+        <Card className={classes.card}>
+            <CardActionArea onClick={handleExpandClick}>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={process.env.PUBLIC_URL + '/images/projects/s7/m1.png'}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.subheader}
+                    </Typography>
+                </CardContent>
+                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                    <CardContent>
+                        <hr/>
+                        {props.content}
+                    </CardContent>
+                </Collapse>
+            </CardActionArea>
+            <CardActions>
+                <IconButton
+                    className={expanded?classes.expandOpen:classes.expand}
+                    onClick={handleExpandClick}>
+                    <ExpandMore/>
+                </IconButton>
+            </CardActions>
+        </Card>
+        </>
+    )
+}
+
+function Education() {
+    return (
+        <Typography variant="body1">2000</Typography>
     )
 }
 
